89a7d161357ffd1f442d7e4605249f49425aa82b
mortynation <nivaasanna24@gmail.com>
2023-07-30 20:04:45 -0400

    added shell script for execute shell in jenkins

---
+++ test.sh
@@ -0,0 +1,20 @@
+#! /bin/bash -x
+
+echo "Build host: $HOSTNAME"
+echo "Build running on ${NODE_NAME}"
+
+CONTAINER_ID=$(docker ps --filter "name=pong_serv_cont" | awk '{print $1}' |grep -v CONTAINER)
+echo $CONTAINER_ID
+kill_container () {
+docker stop $CONTAINER_ID
+docker rm $CONTAINER_ID
+}
+
+run_container () {
+docker run -p 8090:80 -d --name pong_serv_cont pong_serv_image
+}
+docker build -f $WORKSPACE/Dockerfile -t pong_serv_image .
+[ ! -z $CONTAINER_ID ] && kill_container
+run_container
+# Show running containers
+docker ps
